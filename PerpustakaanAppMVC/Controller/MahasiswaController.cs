1c106fccb6fdd66be81865f161c638d92f866ec5
Winda Ardelia Aristawidya <127828701+windaardeliaa@users.noreply.github.com>
2023-11-29 22:51:27 +0700

    Add files via upload

---
+++ PerpustakaanAppMVC/Controller/MahasiswaController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+using System.Windows.Forms;
+using PerpustakaanAppMVC.Model.Entity;
+using PerpustakaanAppMVC.Model.Repository;
+using PerpustakaanAppMVC.Model.Context;
+
+namespace PerpustakaanAppMVC.Controller
+{
+    public class MahasiswaController
+    {
+        private MahasiswaRepository _repository;
+        private DbContext _context;
+
+        public MahasiswaController(DbContext context)
+        {
+            _context = context;
+            _repository = new MahasiswaRepository(context);
+        }
+
+        public int Create(Mahasiswa mhs)
+        {
+            int result = 0;
+            // cek npm yang diinputkan tidak boleh kosong
+            if (string.IsNullOrEmpty(mhs.Npm))
+            {
+                MessageBox.Show("NPM harus diisi !!!", "Peringatan",
+                MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return 0;
+            }
+            // cek nama yang diinputkan tidak boleh kosong
+            if (string.IsNullOrEmpty(mhs.Nama))
+            {
+                MessageBox.Show("Nama harus diisi !!!", "Peringatan",
+                MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return 0;
+            }
+            // cek angkatan yang diinputkan tidak boleh kosong
+            if (string.IsNullOrEmpty(mhs.Angkatan))
+            {
+                MessageBox.Show("Angkatan harus diisi !!!", "Peringatan",
+                MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return 0;
+            }
+            // membuat objek context menggunakan blok using
+            using (DbContext context = new DbContext())
+            {
+                // membuat objek class repository
+                _repository = new MahasiswaRepository(context);
+                // panggil method Create class repository untuk menambahkan data
+                result = _repository.Create(mhs);
+            }
+            if (result > 0)
+            {
+                MessageBox.Show("Data mahasiswa berhasil disimpan !", "Informasi",
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+                MessageBox.Show("Data mahasiswa gagal disimpan !!!", "Peringatan",
+                MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            return result;
+        }
+        public int Update(Mahasiswa mhs)
+        {
+            int result = 0;
+
+            try
+            {
+                string query = "UPDATE Mahasiswa SET Nama = @Nama, Angkatan = @Angkatan WHERE Npm = @Npm";
+                using (SQLiteCommand cmd = new SQLiteCommand(query, _context.Conn))
+                {
+                    cmd.Parameters.AddWithValue("@Npm", mhs.Npm);
+                    cmd.Parameters.AddWithValue("@Nama", mhs.Nama);
+                    cmd.Parameters.AddWithValue("@Angkatan", mhs.Angkatan);
+
+                    result = cmd.ExecuteNonQuery();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error updating Mahasiswa: {ex.Message}");
+            }
+
+            return result;
+        }
+        public int Delete(Mahasiswa mhs)
+        {
+            int result = 0;
+
+            try
+            {
+                string query = "DELETE FROM Mahasiswa WHERE Npm = @Npm";
+                using (SQLiteCommand cmd = new SQLiteCommand(query, _context.Conn))
+                {
+                    cmd.Parameters.AddWithValue("@Npm", mhs.Npm);
+
+                    result = cmd.ExecuteNonQuery();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error deleting Mahasiswa: {ex.Message}");
+            }
+
+            return result;
+        }
+        public List<Mahasiswa> ReadByNama(string nama)
+        {
+            // membuat objek collection
+            List<Mahasiswa> list = new List<Mahasiswa>();
+            // membuat objek context menggunakan blok using
+            using (DbContext context = new DbContext())
+            {
+                // membuat objek dari class repository
+                _repository = new MahasiswaRepository(context);
+                // panggil method GetByNama yang ada di dalam class repository
+                list = _repository.ReadByNama(nama);
+            }
+            return list;
+        }
+        public List<Mahasiswa> ReadAll()
+        {
+            // membuat objek collection
+            List<Mahasiswa> list = new List<Mahasiswa>();
+            // membuat objek context menggunakan blok using
+            using (DbContext context = new DbContext())
+            {
+                // membuat objek dari class repository
+                _repository = new MahasiswaRepository(context);
+                // panggil method GetAll yang ada di dalam class repository
+                list = _repository.ReadAll();
+            }
+            return list;
+        }
+    }
+}
